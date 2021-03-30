8d7db0ace4a354be041e7bd73276c81518c80616
Kanchan Chauhan <kanchan827@gmail.com>
2021-03-29 22:26:45 -0700

    set up the env

---
+++ dump/client/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { GoogleMap } from '@react-google-'
+
+const App = () => {
+
+
+
+    return(
+      <div>
+        MAP
+      </div>
+    )
+
+}
+
+export default App;
